a5b8067e49a5f200b6011d48f8ab45632530573d
Alexvonrass <alexvonrass@gmail.com>
2024-04-25 10:12:41 +0200

    add sample files

--- ridedott-pmc-hackathon/ridedott_pmc_hackathon/answer_question_using_context.py
+++ ridedott-pmc-hackathon/ridedott_pmc_hackathon/answer_question_using_context.py
@@ -1,3 +1,9 @@
+from pathlib import Path
+
 import vertexai
 from google.api_core.client_options import ClientOptions
 from vertexai.preview.generative_models import GenerativeModel, Image
+
+if __name__ == "__main__":
+    with open("a.txt"):
+        pass
