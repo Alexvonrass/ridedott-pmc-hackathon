25ca82bf042de18b02e9a149ffa85e9e388503a1
Alexvonrass <alexvonrass@gmail.com>
2024-04-25 14:19:24 +0200

    add readme

---
+++ ridedott-pmc-hackathon/ridedott_pmc_hackathon/audio_to_text.py
@@ -0,0 +1,55 @@
+from pathlib import Path
+from typing import ByteString
+
+import vertexai
+from google.cloud.speech_v2 import SpeechClient
+from google.cloud.speech_v2.types import cloud_speech
+from google.oauth2 import service_account
+from vertexai.preview.generative_models import GenerativeModel, Image
+
+SERVICE_ACCOUNT_FILE = Path(__file__).resolve().parents[1] / "sa" / "sa.json"
+
+# Authenticate using the service account key file
+credentials = service_account.Credentials.from_service_account_file(
+    SERVICE_ACCOUNT_FILE,
+    scopes=["https://www.googleapis.com/auth/cloud-platform"],
+)
+
+
+def audio_to_text(
+    project_id: str,
+    audio: str,
+) -> cloud_speech.RecognizeResponse:
+    """Transcribe an audio file."""
+    # Instantiates a client
+    client = SpeechClient(credentials=credentials)
+
+    config = cloud_speech.RecognitionConfig(
+        auto_decoding_config=cloud_speech.AutoDetectDecodingConfig(),
+        # Language codes is optional
+        language_codes=["nl-NL"],
+        model="long",
+    )
+
+    request = cloud_speech.RecognizeRequest(
+        recognizer=f"projects/{project_id}/locations/global/recognizers/_",
+        config=config,
+        content=audio,
+    )
+
+    # Transcribes the audio into text
+    response = client.recognize(request=request)
+    # language = response.results
+    print(response)
+    for result in response.results:
+        print(f"Transcript: {result.alternatives[0].transcript}")
+
+    return response
+
+
+if __name__ == "__main__":
+    # Reads a file as bytes
+    input_dir = Path(__file__).resolve().parents[1] / "input_data"
+    with open(input_dir / "audio" / "AUDIO4.m4a", "rb") as f:
+        audio = f.read()
+    audio_to_text("qwiklabs-gcp-02-08a8c713b151", audio)
