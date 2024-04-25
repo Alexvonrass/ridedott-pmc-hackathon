1b19b27ea190f9dfc5497a949af3d5a975f8caf6
Alexvonrass <alexvonrass@gmail.com>
2024-04-25 10:42:25 +0200

    add SA

--- ridedott-pmc-hackathon/ridedott_pmc_hackathon/answer_question_using_context.py
+++ ridedott-pmc-hackathon/ridedott_pmc_hackathon/answer_question_using_context.py
@@ -1,13 +1,27 @@
+import os
+import pdb
 from pathlib import Path
 
 import vertexai
 from google.api_core.client_options import ClientOptions
+from google.oauth2 import service_account
 from vertexai.preview.generative_models import GenerativeModel, Image
 
 if __name__ == "__main__":
     PROJECT_ID = "qwiklabs-gcp-02-08a8c713b151"
     REGION = "europe-west4"
-    vertexai.init(project=PROJECT_ID, location=REGION)
+    SERVICE_ACCOUNT_FILE = (
+        Path(__file__).resolve().parents[1] / "sa" / "sa.json"
+    )
+
+    # Authenticate using the service account key file
+    credentials = service_account.Credentials.from_service_account_file(
+        SERVICE_ACCOUNT_FILE,
+        scopes=["https://www.googleapis.com/auth/cloud-platform"],
+    )
+    print(credentials)
+
+    vertexai.init(project=PROJECT_ID, location=REGION, credentials=credentials)
     input_dir = Path(__file__).resolve().parents[1] / "input_data"
     with open(
         input_dir
@@ -34,13 +48,5 @@ if __name__ == "__main__":
         using the above context, answer the following question:
         {questions}"""
 
-    generative_multimodal_model = GenerativeModel(
-        "gemini-1.5-pro-preview-0409"
-    )
     response = generative_multimodal_model.generate_content(model_prompt)
     print(response)
-    # completion = await generative_multimodal_model.generate_content_async(
-    #     stream=True, contents="hello"
-    # )
-    #
-    # iter = completion.__aiter__()
